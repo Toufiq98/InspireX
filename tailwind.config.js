00c5a0fa8d70a23a5dcb9c76624b3b99fa8f4af4
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-04-03 23:31:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx,html}"
+    ],
+    safelist: [
+      "fa", "fas", "far", "fab",
+      "fa-home", "fa-user", "fa-twitter", "fa-check","fa-facebook"
+    ],
+  };
+  
