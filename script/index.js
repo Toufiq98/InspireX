a3f83f5d965829aab30c8964567ff82af8d59b9b
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-04-14 13:24:19 +0600

    contact added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
 document.getElementById("contact-button").addEventListener("click", function() {
     window.location.href = "contact.html";
 });
+document.getElementById("touch-button").addEventListener("click", function() {
+    window.location.href = "contact.html";
+});
+document.getElementById("touch-button1").addEventListener("click", function() {
+    window.location.href = "contact.html";
+});
