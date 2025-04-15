476ab8a1412ebe67e77f0106e071437be0ba1f29
Toufiqur Rahman <toufiquer1998@gmail.com>
2025-04-16 00:18:19 +0600

    mobile version

--- script/index.js
+++ script/index.js
@@ -7,3 +7,8 @@ document.getElementById("touch-button").addEventListener("click", function() {
 document.getElementById("touch-button1").addEventListener("click", function() {
     window.location.href = "contact.html";
 });
+
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    const menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden');
+  });
